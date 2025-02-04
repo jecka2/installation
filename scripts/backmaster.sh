d905fad04f9cefeee9a26c7c5e449089262bf93a
jecka <kalininea88@gmail.com>
2025-02-04 15:09:17 +0300

    Made some imp

--- scripts/backmaster.sh
+++ scripts/backmaster.sh
@@ -3,11 +3,31 @@
 apt update && apt upgrade -y
 
 
+# Директория, в которой находятся файлы
+directory="/etc/netplan"
+
+# Переходим в указанную директорию
+cd "$directory" || exit 1
+
+
+# Получаем список всех файлов в этой директории
+files=(*)
+
+# Цикл по всем файлам
+for file in "${files[@]}"; do
+    # Если текущий элемент является файлом
+    if [[ -f "$file" ]]; then
+        # Формируем новое имя файла
+        new_file="${file%.yml}.yml.bak"
+            # Переименовываем файл
+            mv -n "$file" "$new_file"
+       fi
+done
 # Переменные для настройки сети
-IP_ADDRESS="192.168.1.22"   # Статический IP-адрес
+IP_ADDRESS="192.168.1.141"   # Статический IP-адрес
 NETMASK="24"      # Маска подсети
-GATEWAY="192.168.0.1"        # Шлюз по умолчанию
-DNS_SERVERS="192.168.1.147 192.168.1.146" # DNS-серверы
+GATEWAY="192.168.1.1"        # Шлюз по умолчанию
+DNS_SERVERS="192.168.1.147,192.168.1.146" # DNS-серверы
 
 #Поиск текущего сетевого интерфейса
 INT=$(ip link show | grep '^[0-9]*:' | awk '{print $2}' |grep -v '^lo:$')
@@ -27,14 +47,17 @@ network:
     $INT
       dhcp4: no
       addresses: [$IP_ADDRESS/$NETMASK]
-      gateway4: $GATEWAY
+      routes:
+      - to: default
+        via: $GATEWAY
       nameservers:
         addresses: [$DNS_SERVERS]
 EOF
 
-# Применяем изменения
-sudo netplan apply
 
+# Применяем изменения
+#sudo netplan apply
+chmod 600 $INTERFACE_FILE
 
 ## Установка MySQL Server
 apt-get install mysql-server git  -y
@@ -46,8 +69,8 @@ systemctl start mysql
 
 # Создание пользователя для репликации
 SQL_COMMANDS="
-CREATE USER 'replication_user'@'%' IDENTIFIED BY 'password';
-GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%';
+CREATE USER 'jecka'@'%' IDENTIFIED BY '123qweASD!';
+GRANT ALL PRIVILEGES ON *.* TO 'jecka'@'%';
 FLUSH PRIVILEGES;
 "
 mysql -u root -e "${SQL_COMMANDS}" 
@@ -55,3 +78,6 @@ mysql -u root -e "${SQL_COMMANDS}"
 systemctl stop mysql
 cp /tmp/mysqld_main.cnf  /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl start mysql
+
+reboot now
+
--- scripts/backrepl.sh
+++ scripts/backrepl.sh
@@ -3,10 +3,30 @@
 apt update && apt upgrade -y
 
 
+# Директория, в которой находятся файлы
+directory="/etc/netplan"
+
+# Переходим в указанную директорию
+cd "$directory" || exit 1
+
+
+# Получаем список всех файлов в этой директории
+files=(*)
+
+# Цикл по всем файлам
+for file in "${files[@]}"; do
+    # Если текущий элемент является файлом
+    if [[ -f "$file" ]]; then
+        # Формируем новое имя файла
+        new_file="${file%.yml}.yml.bak"
+            # Переименовываем файл
+            mv -n "$file" "$new_file"
+       fi
+done
 # Переменные для настройки сети
-IP_ADDRESS="192.168.1.22"   # Статический IP-адрес
+IP_ADDRESS="192.168.1.142"   # Статический IP-адрес
 NETMASK="24"      # Маска подсети
-GATEWAY="192.168.0.1"        # Шлюз по умолчанию
+GATEWAY="192.168.1.1"        # Шлюз по умолчанию
 DNS_SERVERS="192.168.1.147,192.168.1.146" # DNS-серверы
 
 #Поиск текущего сетевого интерфейса
@@ -27,13 +47,17 @@ network:
     $INT
       dhcp4: no
       addresses: [$IP_ADDRESS/$NETMASK]
-      gateway4: $GATEWAY
+      routes:
+      - to: default
+        via: $GATEWAY
       nameservers:
         addresses: [$DNS_SERVERS]
 EOF
 
+
 # Применяем изменения
-sudo netplan apply
+#sudo netplan apply
+chmod 600 $INTERFACE_FILE
 
 
 ## Установка MySQL Server
@@ -41,26 +65,48 @@ apt-get install mysql-server git  -y
 
 # Запуск службы MySQL
 systemctl start mysql
- 
-echo "Укажи имя bin log которое было указано в таблице ранее"
-read binlog
-echo "Укажи позицию из таблицы котороая была указана ранее"
-read pos
 
-# Создание пользователя для репликации
+
 SQL_COMMANDS="
-STOP SLAVE;
-CHANGE MASTER TO MASTER_HOST='192.168.1.21', MASTER_USER='replication_user', MASTER_PASSWORD='password', MASTER_LOG_FILE='$binlog', MASTER_LOG_POS=$pos, GET_MASTER_PUBLIC_KEY = 1;
-START SLAVE;
-show replica status\G
-
-# GTID
-STOP REPLICA;
-CHANGE REPLICATION SOURCE TO SOURCE_HOST='mysql8master', SOURCE_USER='repl', SOURCE_PASSWORD='oTUSlave#2020', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;
-START REPLICA;
+CREATE USER 'replication'@'%' IDENTIFIED BY 'password';
+GRANT REPLICATION SLAVE ON *.* TO 'replication'@'%';
+FLUSH PRIVILEGES;
+SHOW MASTER STATUS;
 "
-mysql -u root -e "${SQL_COMMANDS}" 
+mysql -u jecka -h 192.168.1.141 -p123qweASD! -e "${SQL_COMMANDS}"
+
+
+ 
+#SQL_COMMANDS="
+#CREATE USER 'replication'@'%' IDENTIFIED BY 'password';
+#GRANT REPLICATION SLAVE ON *.* TO 'replication'@'%';
+#FLUSH PRIVILEGES;
+#"
+
+
+# Получаем информацию о позиции бинлога
+MASTER_LOG_FILE=$(mysql -ujecka -h192.168.1.141 -p123qweASD!  -e "SHOW MASTER STATUS\G" | grep File | awk '{print $2}')
+MASTER_LOG_POS=$(mysql -ujecka -h192.168.1.141 -p123qweASD! -e "SHOW MASTER STATUS\G" | grep Position | awk '{print $2}')
+
+# Сохраняем данные в файл
+echo "Master Log File: ${MASTER_LOG_FILE}" > /tmp/master_info.txt
+echo "Master Log Pos: ${MASTER_LOG_POS}" >> /tmp/master_info.txt
+
+ 
 
 systemctl stop mysql
 cp /tmp/mysqld_slave.cnf  /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl start mysql
+
+mysql -u root -p <<EOF
+CHANGE MASTER TO
+MASTER_HOST='192.168.1.141',
+MASTER_USER='replication',
+MASTER_PASSWORD='password',
+MASTER_LOG_FILE='$(cat /tmp/master_info.txt | head -n 1 | cut -d ":" -f 2)',
+MASTER_LOG_POS=$(cat /tmp/master_info.txt | tail -n 1 | cut -d ":" -f 2);
+START SLAVE;
+EOF
+
+# Проверка статуса репликации
+mysql -u root -p -e "SHOW SLAVE STATUS\G"
